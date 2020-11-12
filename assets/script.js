d9677dac71b54852b9bac94abd35426de459979c
zsofia <50777550+zsofia85@users.noreply.github.com>
2020-11-12 22:18:26 +0100

    styling changes

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+
+    var fold = document.getElementById("ViewBtn");
+    var i;
+
+    for (i = 0; i < fold.length; i++) {
+        fold[i].addEventListener("click", function(){
+            this.classList.toggle("active");
+            
+            var gallery = this.nextElementSibling;
+            if (gallery.style.display === "block") {
+                gallery.style.display = "none";
+            } else {
+                gallery.style.display = "block";
+            }
+        });
+    }
