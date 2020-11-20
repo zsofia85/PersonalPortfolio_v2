0cb703159b4684cceeceddc6f7b6f787db679729
zsofia <50777550+zsofia85@users.noreply.github.com>
2020-11-20 10:29:20 +0100

    styling

--- assets/script.js
+++ assets/script.js
@@ -3,10 +3,10 @@
     var i;
 
     for (i = 0; i < fold.length; i++) {
-        fold[i].addEventListener("click", function(){
+        fold[i].addEventListener("click", function() {
             this.classList.toggle("active");
 
-            var gallery = this.nextElementSibling;
+            var gallery = this.nextElement;
             if (gallery.style.display === "block") {
                 gallery.style.display = "none";
             } else {
